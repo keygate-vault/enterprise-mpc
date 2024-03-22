f8794e3f3c8b1ed791cf6598dc1956bfab207d0a
Phillip Nunes <phillipnunes86@gmail.com>
2024-03-22 16:09:36 -0300

    commenting code

--- src/iwallet_frontend/src/App.tsx
+++ src/iwallet_frontend/src/App.tsx
@@ -1,32 +1,31 @@
 import { useState } from "react";
-// @ts-ignore
-import { iwallet_backend } from "declarations/iwallet_backend";
+// import { iwallet_backend } from "declarations/iwallet_backend";
 import { SignIn } from './components/Forms/SignIn/Index'
 
 function App() {
   const [registrationMessage, setRegistrationMessage] = useState("");
   const [lookupResult, setLookupResult] = useState("");
 
-  async function handleRegister(event) {
-    event.preventDefault();
-    const email = event.target.elements.email.value;
-    const name = event.target.elements.name.value;
-    const message = await iwallet_backend.register(email, name);
-    setRegistrationMessage(message);
-    return false;
-  }
-
-  async function handleLookup(event) {
-    event.preventDefault();
-    const email = event.target.elements.lookupEmail.value;
-    const result = await iwallet_backend.lookup(email);
-    if (result.ok) {
-      setLookupResult(`Name: ${result.ok}`);
-    } else {
-      setLookupResult(`Error: ${result.err}`);
-    }
-    return false;
-  }
+  // async function handleRegister(event) {
+  //   event.preventDefault();
+  //   const email = event.target.elements.email.value;
+  //   const name = event.target.elements.name.value;
+  //   const message = await iwallet_backend.register(email, name);
+  //   setRegistrationMessage(message);
+  //   return false;
+  // }
+  //
+  // async function handleLookup(event) {
+  //   event.preventDefault();
+  //   const email = event.target.elements.lookupEmail.value;
+  //   const result = await iwallet_backend.lookup(email);
+  //   if (result.ok) {
+  //     setLookupResult(`Name: ${result.ok}`);
+  //   } else {
+  //     setLookupResult(`Error: ${result.err}`);
+  //   }
+  //   return false;
+  // }
 
   const onSignIn = (values: any) => {
     console.log('onSignIn', values)
