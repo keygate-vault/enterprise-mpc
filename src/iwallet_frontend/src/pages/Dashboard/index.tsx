615d152a8721d2a31a995a74e0b36489d809c1dc
Vicente Bermúdez <vicenteb@web3pro.com>
2024-03-27 12:56:14 -0300

    vaults design

--- src/iwallet_frontend/src/pages/Dashboard/index.tsx
+++ src/iwallet_frontend/src/pages/Dashboard/index.tsx
@@ -1,70 +1,104 @@
-import { Button, Modal, notification } from "antd";
+import { Alert, Button, Card, Modal, notification } from "antd";
 import Table, { ColumnProps } from "antd/es/table";
 import { useEffect, useState } from "react";
-import { iwallet_backend } from "../../../../declarations/iwallet_backend";
-import CreateWalletModal from "../Wallets/Create";
-import { useNavigate, useParams } from "react-router-dom";
+import CreateVaultModal from "../Vaults/Create";
+import { useNavigate } from "react-router-dom";
 
-type Wallet = {
+type Vault = {
   id: string;
-  publicKey: string;
+  name: string;
+  available: string;
+  blockchains: string[];
 };
 
-const columns: ColumnProps<Wallet>[] = [
+const columns: ColumnProps<Vault>[] = [
   {
-    title: "ID",
-    dataIndex: "id",
-    key: "id",
+    title: "NAME",
+    dataIndex: "name",
+    key: "name",
+    className: "font-semibold",
   },
   {
-    title: "PUBLIC KEY",
-    dataIndex: "publicKey",
-    key: "publicKey",
+    title: "AVAILABLE",
+    dataIndex: "available",
+    key: "available",
+  },
+  {
+    title: "BLOCKCHAINS",
+    dataIndex: "blockchains",
+    key: "blockchains",
+    render: (blockchains: string[]) => {
+      // show eth and btc icon accordingly (can show both if both are present in the array)
+      // use svgs
+      return (
+        <div>
+          {blockchains.map((blockchain) => {
+            return (
+              <img
+                key={blockchain}
+                src={`/public/${blockchain}.svg`}
+                alt={blockchain}
+                className="w-6 h-6 mr-[-8px]"
+              />
+            );
+          })}
+        </div>
+      );
+    },
+  },
+];
+
+const mockVaults: Vault[] = [
+  {
+    id: "1",
+    name: "Funding",
+    available: "$32.00",
+    blockchains: ["eth", "btc"],
+  },
+  {
+    id: "2",
+    name: "Investments",
+    available: "$1,000.4",
+    blockchains: ["btc"],
+  },
+  {
+    id: "3",
+    name: "Savings",
+    available: "$5,000.2",
+    blockchains: ["eth"],
   },
 ];
 
 const Dashboard = () => {
-  const [dataSource, setDataSource] = useState<Wallet[]>([]);
+  const [dataSource, setDataSource] = useState<Vault[]>(mockVaults);
   const [api] = notification.useNotification();
   const [isLoading, setIsLoading] = useState(false);
   const [open, setOpen] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
-    fetchWallets();
+    fetchVaults();
   }, []);
 
-  const fetchWallets = async () => {
+  const fetchVaults = async () => {
     try {
       setIsLoading(true);
-      const response = await iwallet_backend.getAll();
-      setIsLoading(false);
-      if (response) {
-        console.log("response", response);
-        const wallets = response.map((wallet) => ({
-          id: wallet.id.toText(),
-          publicKey: wallet.publicKey,
-        }));
-
-        setDataSource(wallets);
-      } else {
-        api.error({
-          message: "Failed to fetch wallets",
-          description: "Please try again later.",
-          placement: "bottom",
-        });
-      }
+      // Simulating an API call with mock data
+      setTimeout(() => {
+        setIsLoading(false);
+        setDataSource(mockVaults);
+      }, 1000);
     } catch (error) {
       api.error({
-        message: "Error fetching wallets",
+        message: "Error fetching vaults",
         description: "Please try again later.",
         placement: "bottom",
       });
     }
   };
 
-  const onRowClick = (record: Wallet) => {
-    navigate(`/wallets/${record.id}`);
+  const onRowClick = (record: Vault) => {
+    navigate(`/vaults/${record.id}`);
   };
 
   const showModal = () => {
@@ -73,11 +107,23 @@ const Dashboard = () => {
 
   return (
     <main className="flex min-h-screen flex-col items-left p-16">
-      <h1 className="text-3xl font-bold">Wallets</h1>
+      <h1 className="text-3xl font-bold">Vaults</h1>
+      {/* <Alert
+        message="What is a Vault?"
+        description={
+          <>
+            <p>
+              A Vault account is an easy way to separate and maintain security
+              boundaries across assets.
+            </p>
+          </>
+        }
+        type="info"
+        showIcon
+        style={{ marginTop: 16, marginBottom: 16 }}
+      /> */}
       <div className="flex flex-row justify-between">
-        <p className="text-lg">
-          Create and view custodial wallets for your users.
-        </p>
+        <p className="text-lg">Manage and secure your assets.</p>
         <Button
           type="primary"
           className="mt-4"
@@ -85,26 +131,31 @@ const Dashboard = () => {
             showModal();
           }}
         >
-          New wallet
+          New vault
         </Button>
       </div>
-      <CreateWalletModal
+
+      <CreateVaultModal
         visible={open}
         setVisible={setOpen}
-        refreshWallets={() => {
-          fetchWallets();
+        refreshVaults={() => {
+          fetchVaults();
         }}
       />
-      <Table
-        loading={isLoading}
-        rowKey="id"
-        rowClassName="cursor-pointer"
-        dataSource={dataSource}
-        columns={columns}
-        onRow={(record) => ({
-          onClick: () => onRowClick(record),
-        })}
-      />
+      <Card>
+        <Table
+          loading={isLoading}
+          rowKey="id"
+          rowClassName="cursor-pointer"
+          dataSource={dataSource}
+          showHeader={false}
+          columns={columns}
+          pagination={false}
+          onRow={(record) => ({
+            onClick: () => onRowClick(record),
+          })}
+        />
+      </Card>
     </main>
   );
 };
---
+++ src/iwallet_frontend/src/pages/Vaults/Create/index.tsx
@@ -0,0 +1,137 @@
+"use client";
+import { Button, Modal, Typography, Steps } from "antd";
+import { useState } from "react";
+import { iwallet_backend } from "../../../../../declarations/iwallet_backend";
+
+const { Title, Paragraph } = Typography;
+const { Step } = Steps;
+
+export default function CreateVaultModal({
+  visible,
+  setVisible,
+  refreshVaults,
+}: {
+  visible: boolean;
+  setVisible: (visible: boolean) => void;
+  refreshVaults: () => void;
+}) {
+  const [loading, setLoading] = useState(false);
+  const [currentStep, setCurrentStep] = useState(0);
+
+  const handleOk = async () => {
+    setLoading(true);
+    await iwallet_backend.register();
+    setVisible(false);
+    setLoading(false);
+    refreshVaults();
+  };
+
+  const handleCancel = () => {
+    setVisible(false);
+  };
+
+  const handleNext = () => {
+    setCurrentStep((prevStep) => prevStep + 1);
+  };
+
+  const handlePrev = () => {
+    setCurrentStep((prevStep) => prevStep - 1);
+  };
+
+  const steps = [
+    {
+      title: "Introduction",
+      content: (
+        <div>
+          <Paragraph>
+            Welcome to the custodial wallet creation wizard! This wizard will
+            guide you through the process of generating a new custodial wallet
+            that you can use in your APIs, frontends, and mobile apps.
+          </Paragraph>
+          <Paragraph>
+            Custodial wallets provide a secure and convenient way to manage
+            digital assets on behalf of your users. By creating a custodial
+            wallet, you can easily integrate blockchain functionality into your
+            applications without the need for users to manage their own private
+            keys.
+          </Paragraph>
+        </div>
+      ),
+    },
+    {
+      title: "Benefits",
+      content: (
+        <div>
+          <Paragraph>
+            Here are some key benefits of using custodial wallets:
+          </Paragraph>
+          <ul>
+            <li>Enhanced security through centralized key management</li>
+            <li>
+              Simplified user experience for interacting with digital assets
+            </li>
+            <li>Seamless integration with your existing infrastructure</li>
+            <li>Reduced complexity and development effort</li>
+          </ul>
+        </div>
+      ),
+    },
+    {
+      title: "Confirmation",
+      content: (
+        <div>
+          <Paragraph>
+            You are now ready to generate your new custodial wallet. Please
+            review the following information before proceeding:
+          </Paragraph>
+          <ul>
+            <li>
+              The generated wallet will be securely stored and managed by our
+              system
+            </li>
+            <li>
+              You will receive an API key and documentation to interact with the
+              wallet
+            </li>
+            <li>
+              The wallet's private key will be encrypted and kept confidential
+            </li>
+          </ul>
+          <Paragraph>
+            If you agree with the above terms, click "Generate Wallet" to create
+            your new custodial wallet.
+          </Paragraph>
+        </div>
+      ),
+    },
+  ];
+
+  return (
+    <Modal
+      centered={true}
+      title="Wallet Creation"
+      open={visible}
+      onCancel={handleCancel}
+      footer={[
+        <Button key="back" onClick={handlePrev} disabled={currentStep === 0}>
+          Previous
+        </Button>,
+        <Button
+          key="next"
+          type="primary"
+          loading={loading}
+          onClick={currentStep === steps.length - 1 ? handleOk : handleNext}
+        >
+          {currentStep === steps.length - 1 ? "Generate Wallet" : "Next"}
+        </Button>,
+      ]}
+    >
+      <Steps current={currentStep} style={{ marginBottom: 15, marginTop: 24 }}>
+        {steps.map((step) => (
+          <Step key={step.title} title={step.title} />
+        ))}
+      </Steps>
+      <div className="p-8">{steps[currentStep].content}</div>
+    </Modal>
+  );
+}
